f48c435f7e4648b09c896c2ca5a100c3ef58db5c
danielecarpentiero <carpentierodaniele@gmail.com>
2023-12-05 17:22:09 +0100

    feat: resolution

--- main.mjs
+++ main.mjs
@@ -3,14 +3,14 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      list: null,
+      list: [],
     };
   },
   mounted() {
     for (let i = 0; i < 10; i++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((success) => (this.list = success.data.response));
+        .then((success) => this.list.push(success.data.response));
     }
   },
 }).mount("#app");
