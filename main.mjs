77306b4d50d7e917d7a73672f0abd61895dc1c39
danielecarpentiero <carpentierodaniele@gmail.com>
2023-12-05 16:50:43 +0100

    feat: list

--- main.mjs
+++ main.mjs
@@ -7,8 +7,10 @@ createApp({
     };
   },
   mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((success) => (this.list = success.data.response));
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((success) => (this.list = success.data.response));
+    }
   },
 }).mount("#app");
